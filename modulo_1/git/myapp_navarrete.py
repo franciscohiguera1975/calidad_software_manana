3866b7c68a8a73c555881c4cb2d8b9b50d5aeeb4
natcore-tech <asesoriaelectr@gmail.com>
2025-10-03 10:01:37 -0500

    Prueba2

--- modulo_1/git/myapp_navarrete.py
+++ modulo_1/git/myapp_navarrete.py
@@ -2,3 +2,6 @@ print("PRUEBA DESDE CALIDAD DEL SOFTWARE")
 materia = "Calidad del Software"
 if (materia):
     print("Estoy practicando git en: ", materia)
+
+print("PRUEBA DESDE CALIDAD DEL SOFTWARE")
+print("PRUEBA DESDE CALIDAD DEL SOFTWARE")
